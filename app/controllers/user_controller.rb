ada0117dbfa14b2499c6ec07fb0db4dcd5d54503
Will Cook <nzwillcook@gmail.com>
2016-08-06 11:22:53 +0100

    user has correct permissions to other users and a profile picture

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,23 @@
 class UserController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :user_authorization, only: [:show]
+
+
   def show
     @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
   end
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  private 
+  # User can only see his/her profile
+    def user_authorization
+      redirect_to(root_path) unless current_user.id.to_s == params[:id]
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806063146) do
+ActiveRecord::Schema.define(version: 20160806072008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20160806063146) do
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "profile_picture"
     t.string   "contact_number"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
