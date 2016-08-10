de1d438ea7a104d73d265d8b6d341fdb78899f9c
Will Cook <nzwillcook@gmail.com>
2016-08-10 18:02:15 +0100

    started work on the js to truncate descriptions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,21 @@
 
 $(document).on('turbolinks:load', function() {
   $('select').select2();
+
+  function truncateOnWord(str, limit) {
+          var trimmable = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u2028\u2029\u3000\uFEFF';
+          var reg = new RegExp('(?=[' + trimmable + '])');
+          var words = str.split(reg);
+          var count = 0;
+          sentance = words.filter(function(word) {
+              count += word.length;
+              return count <= limit;
+          }).join('');
+          sentance += '...';
+          return sentance;
+      }
+  $texttotruncate = $('p.card-text').text() 
+  $(alert(truncateOnWord($texttotruncate, 120)));
+  $('p.card-text').replaceWith( "<p>" + ($(truncateOnWord($texttotruncate, 120)).text()) + "</p>" );    
 });
+
