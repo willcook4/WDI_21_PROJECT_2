f74d003b40e93c21f1f08e0557e6c7edf660034b
Will Cook <nzwillcook@gmail.com>
2016-08-10 15:34:48 +0100

    corrected the profileuploader rb for aws

--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -7,7 +7,7 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -17,11 +17,9 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("assets/images/fallback/profile/" + [version_name, "blank-profile.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_path("fallback/profile/" + [version_name, "blank-profile.png"].compact.join('_'))
   end
 
   # Process files as they are uploaded:
