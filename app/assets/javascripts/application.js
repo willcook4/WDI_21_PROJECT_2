f2348cc5fd26ce3e6905683dc16a4d6d1b07f3ab
Will Cook <nzwillcook@gmail.com>
2016-08-11 11:08:15 +0100

    Message page styled

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,17 +20,17 @@
 $(document).on('turbolinks:load', function() {
   $('select').select2();
 
-  function truncateOnWord(str, limit) {
-    var trimmable = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u2028\u2029\u3000\uFEFF';
-    var reg = new RegExp('(?=[' + trimmable + '])');
-    var words = str.split(reg);
-    var count = 0;
-    sentance = words.filter(function(word) {
-        count += word.length;
-        return count <= limit;
-    }).join('');
-    return sentance + "...";
-  }
+  // function truncateOnWord(str, limit) {
+  //   var trimmable = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u2028\u2029\u3000\uFEFF';
+  //   var reg = new RegExp('(?=[' + trimmable + '])');
+  //   var words = str.split(reg);
+  //   var count = 0;
+  //   sentance = words.filter(function(word) {
+  //       count += word.length;
+  //       return count <= limit;
+  //   }).join('');
+  //   return sentance + "...";
+  // }
 
   function willtruncation(string) {
     // console.log(string.text());
