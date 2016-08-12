2a2efc7b26fc3003fd8fa5d94938fb22319dd379
Will Cook <nzwillcook@gmail.com>
2016-08-12 01:21:22 +0100

    styled the front page

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -2,9 +2,13 @@ class TagController < ApplicationController
   
   def index
     @tags = Tag.all
+    @users = User.all
+    @things = Things.all
   end
 
   def show
+    @users = User.all
+    @tags = Tag.all
     @tag = Tag.find(params[:id])
   end
 end
